8e85668180f594437eef12b5c06aa3692c7034fc
lenguyentri1999 <michaelle7312@gmail.com>
2017-11-12 03:45:40 -0500

    finished backbone for feedback page

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('app', ['ionic','app.routes','app.controllers','app.services','firebase','firebaseConfig', 'ngStorage'])
+angular.module('app', ['ionic','app.routes','app.controllers','app.services','firebase','firebaseConfig', 'ngStorage', 'ionic-rating-stars'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
