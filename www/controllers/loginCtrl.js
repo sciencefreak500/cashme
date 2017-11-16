e8788911ee198c8ed6dc4f9aa49c37231572ed84
Bao Thien Pham <baotpham7@gmail.com>
2017-11-16 01:25:13 -0500

    clean up a little with login

--- www/controllers/loginCtrl.js
+++ www/controllers/loginCtrl.js
@@ -1,39 +1,45 @@
 app.controller('loginPageCtrl', ['$scope','$state','$http','$timeout',
 
   function ($scope, $state,$http,$timeout) {
-  	console.log("were good now");
 
+		//user login credential info
   	$scope.user = {email:"", password:""};
 
   	firebase.auth().onAuthStateChanged(function(user) {
-	  if (user) {
-	    $state.go('settings');
-	    $timeout(function(){$scope.$apply();});
-	  }
-	});
+	  	if (user) {
+	    	$state.go('settings');
+	    	$timeout(function(){$scope.$apply();});
+	  	}
+		});
 
-    $scope.error = {message:""};
+		//error message for login
+		$scope.error = {message:""};
+
   	$scope.loginUser = function(){
-  		console.log("login user");
-  		firebase.auth().createUserWithEmailAndPassword($scope.user.email, $scope.user.password).then(function(user){
-  			console.log("success",user);
+  		console.log("loginCtrl.js - login user");
+			
+			//create new account 
+			firebase.auth().createUserWithEmailAndPassword($scope.user.email, $scope.user.password).then(function(user){
+  			console.log("loginCtrl.js - successful logged in for new user", user);
   			userobj = {
   				uid: user.uid,
   				email: user.email
   			};
   			firebase.database().ref('Users').child(user.uid).update(userobj).then(function(x){
-  				console.log("created obj");
+  				console.log("loginCtrl.js - created a new user");
   				$state.go('settings');
   			});
 
   		}).catch(function(error){
-  			console.log("error", error);
-  			if(error.code == "auth/email-already-in-use"){
+  			console.log("loginCtrl.js - error log in ", error);
+				
+				//sign in with exiting account
+				if(error.code == "auth/email-already-in-use"){
   				firebase.auth().signInWithEmailAndPassword($scope.user.email, $scope.user.password).then(function(user){
-  					console.log("success",user);
+  					console.log("loginCtrl.js - success logged in with existing user",user);
   					$state.go('settings');
   				}).catch(function(err){
-            console.log("trying to sign-in,", err.message);
+            console.log("loginCtrl.js - ", err.message);
             $scope.error.message = err.message;
             $timeout(function(){$scope.$apply();});
           });
