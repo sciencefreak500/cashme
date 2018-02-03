288a44cb63d00252b558865abe10fe23ae1d7869
Bao Thien Pham <baotpham7@gmail.com>
2018-02-03 12:51:57 -0500

    things

--- www/controllers/walletCtrl.js
+++ www/controllers/walletCtrl.js
@@ -46,6 +46,7 @@ app.controller('walletCtrl', ['$scope','$state','$http','userMoney','$timeout','
       firebase.database().ref("Users").child($scope.userID).update({available: $scope.money.isAvailable});
     };
 
+    
 
     $scope.$on('$ionicView.beforeEnter', function () {
 
@@ -108,10 +109,21 @@ app.controller('walletCtrl', ['$scope','$state','$http','userMoney','$timeout','
       });
     });
 
+    Number.prototype.formatMoney = function(c, d, t){
+      var n = this, 
+      c = isNaN(c = Math.abs(c)) ? 2 : c, 
+      d = d == undefined ? "." : d, 
+      t = t == undefined ? "," : t, 
+      s = n < 0 ? "-" : "", 
+      i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))), 
+      j = (j = i.length) > 3 ? j % 3 : 0;
+      return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+    };
 
     //Update my wallet locally and firebase
     $scope.setMyWallet = function(){
       console.log("walletCtrl.js - setting my wallet");
+      $scope.money.wallet.toFixed(2);
       firebase.database().ref('Users').child($scope.userID).update({
         wallet: $scope.money.wallet
       });
@@ -121,6 +133,7 @@ app.controller('walletCtrl', ['$scope','$state','$http','userMoney','$timeout','
     //update my request locally
     $scope.setMyRequest = function(){
       console.log("walletCtrl.js - setting my request locally");
+      $scope.money.request.toFixed(2);
       userMoney.setRequest($scope.money.request);
     }
 
